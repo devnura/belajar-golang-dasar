43456586f52491bbc537a04ec528d2a95d9ae4a5
Nuranggi Hermawan <nuranggihermawan@gmail.com>
2022-09-24 06:37:20 +0700

    constatnt

--- constant.go
+++ constant.go
@@ -7,4 +7,11 @@ func main() {
 	const lastName string = "Doe"
 	const value = 1000
 	fmt.Println(firstName, lastName, value)
+
+	const (
+		firstName1 string = "Jon"
+		lastName1  string = "Doe"
+		value1     int    = 1000
+	)
+	fmt.Println(firstName1, lastName1, value1)
 }
